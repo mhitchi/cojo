a976341bedc51e904dbaf0093e2c382fe892ab35
mhitchi <mayhitchings@gmail.com>
2020-04-17 13:32:52 -0400

    eventsettingsmodel not working, no errors

--- client/src/components/Calendar/Calendar.js
+++ client/src/components/Calendar/Calendar.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import { Row, Col } from "reactstrap";
 import Sidebar from "../Sidebar";
 import "./CalendarStyles.css";
-import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda } from "@syncfusion/ej2-react-schedule";
+import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, EventSettingsModel, DataManager, WebApiAdaptor } from "@syncfusion/ej2-react-schedule";
+
+// https://www.youtube.com/watch?v=iNkryf_TtZw
 
 class Calendar extends Component {
   constructor(props) {
@@ -12,13 +14,20 @@ class Calendar extends Component {
       }
   }
 
+  localData: EventSettingsModel = {
+    dataSource: [{
+      EndTime: new Date( 2020,3,11,6,30),
+      StartTime: new Date( 2020,3,11,4,0)
+    }]
+  }
+
   render() {
     return (
       <>
       <Row>
         <Sidebar page="myprofile"/>
         <Col md={10}>
-          <ScheduleComponent>
+          <ScheduleComponent currentView="Month">
             <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
           </ScheduleComponent>
         </Col>
