921c2e04386862e0c292a5b3e4a40e4da323e3d5
mhitchi <mayhitchings@gmail.com>
2020-04-17 12:58:46 -0400

    can't add stylesheet

--- client/src/components/Calendar/Calendar.js
+++ client/src/components/Calendar/Calendar.js
@@ -2,134 +2,45 @@ import React, { Component } from "react";
 import { Row, Col } from "reactstrap";
 import Sidebar from "../Sidebar";
 import "./CalendarStyles.css";
-// import ApiCalendar from 'react-google-calendar-api';
-import { format, parse, startOfWeek, addDays, startOfMonth, endOfMonth, endOfWeek, isSameMonth, isSameDay, addMonths, subMonths } from "date-fns";
-import CalendarItem from "./CalendarItem";
-//eventfetch
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment';
+import 'react-big-calendar/lib/sass/styles';
+// import 'react-big-calendar/lib/addons/dragAndDrop/styles';
 
-//https://blog.flowandform.agency/create-a-custom-calendar-in-react-3df1bfd0b728
+const localizer = momentLocalizer(moment)
 
-
-class Calendar extends Component {
-  state = {
-    currentMonth: new Date(),
-    selectedDate: new Date()
-  };
-
-  renderHeader() {
-    const dateFormat = "MMMM yyyy";
-
-    return (
-      <div className="header row flex-middle">
-        <div className="col col-start">
-          <div className="icon" onClick={this.prevMonth}>
-            chevron_left
-          </div>
-        </div>
-        <div className="col col-center">
-          <span>{format(this.state.currentMonth, dateFormat)}</span>
-        </div>
-        <div className="col col-end" onClick={this.nextMonth}>
-          <div className="icon">chevron_right</div>
-        </div>
-      </div>
-    );
-  }
-
-  renderDays() {
-    const dateFormat = "dddd";
-    const days = [];
-    const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    let startDate = startOfWeek(this.state.currentMonth);
-
-    for (let i = 0; i < 7; i++) {
-      days.push(
-        <div className="col col-center" key={i}>
-          {week[i]}
-        </div>
-      );
+const calendar_configuration = {
+  api_key: 'AIzaSyD9CaNV_zJ8KyLbN52dtkHQdYrhTuDFv5s',
+  calendars: [
+    {
+      name: 'demo',
+      url: 'mayhitchings@gmail.com'
     }
+  ],
+  dailyRecurrence: 700,
+  weeklyRecurrence: 500,
+  monthlyRecurrence: 20
+}
 
-    return <div className="days row">{days}</div>;
-  }
-
-  renderCells() {
-    const { currentMonth, selectedDate } = this.state;
-    const monthStart = startOfMonth(currentMonth);
-    const monthEnd = endOfMonth(monthStart);
-    const startDate = startOfWeek(monthStart);
-    const endDate = endOfWeek(monthEnd);
-
-    const dateFormat = "d";
-    const rows = [];
-
-    let days = [];
-    let day = startDate;
-    let formattedDate = "";
-
-    while (day <= endDate) {
-      for (let i = 0; i < 7; i++) {
-        formattedDate = format(day, dateFormat);
-        const cloneDay = day;
-        days.push(
-          <div
-            className={`col cell ${
-              !isSameMonth(day, monthStart)
-                ? "disabled"
-                : isSameDay(day, selectedDate) ? "selected" : ""
-            }`}
-            key={day}
-            onClick={() => this.onDateClick(parse(cloneDay))}
-          >
-            <span className="number">{formattedDate}</span>
-            <span className="bg">{formattedDate}</span>
-          </div>
-        );
-        day = addDays(day, 1);
+class App_Calendar extends Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        events: []
       }
-      rows.push(
-        <div className="row" key={day}>
-          {days}
-        </div>
-      );
-      days = [];
-    }
-    return <div className="body">{rows}</div>;
   }
-
-  onDateClick = day => {
-    this.setState({
-      selectedDate: day
-    });
-  };
-
-  nextMonth = () => {
-    this.setState({
-      currentMonth: addMonths(this.state.currentMonth, 1)
-    });
-  };
-
-  prevMonth = () => {
-    this.setState({
-      currentMonth: subMonths(this.state.currentMonth, 1)
-    });
-  };
 
   render() {
     return (
-      <Row>
-        <Sidebar page="calendar"/>
-        <Col xs={9}>
-          <div className="calendar">
-            {this.renderHeader()}
-            {this.renderDays()}
-            {this.renderCells()}
-          </div>
-        </Col>
-      </Row>
+      <Calendar
+        events={this.state.events}
+        config={calendar_configuration} 
+        localizer={localizer}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}/>
     );
   }
 }
 
-export default Calendar;
+export default App_Calendar;
